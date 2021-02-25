0e6a66bbac88a19db7ede7bb01a0fceefbdb05d8
zyx <3064662835@qq.com>
2021-02-25 17:00:27 +0800

    [feat]fragment学习

--- Helloworld/app/src/main/java/com/zyx/helloworld/fragment/AFragment.java
+++ Helloworld/app/src/main/java/com/zyx/helloworld/fragment/AFragment.java
@@ -1,5 +1,7 @@
 package com.zyx.helloworld.fragment;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +18,15 @@ import org.w3c.dom.Text;
 
 public class AFragment extends Fragment {
     private TextView mTvTitle;
+    private Activity activity;
+
+    public static  AFragment newInstance(String title) {
+        AFragment fragment = new AFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("title", title);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -27,5 +38,28 @@ public class AFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mTvTitle = (TextView) view.findViewById(R.id.tv_title);
+        // 判断activity是否为null
+        if (getActivity() != null) {
+            //...
+            if (getArguments() != null) {
+                mTvTitle.setText(getArguments().getString("title"));
+            }
+        }
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        // 取消异步
     }
 }
--- Helloworld/app/src/main/java/com/zyx/helloworld/fragment/ContainerActivity.java
+++ Helloworld/app/src/main/java/com/zyx/helloworld/fragment/ContainerActivity.java
@@ -1,7 +1,10 @@
 package com.zyx.helloworld.fragment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,11 +15,17 @@ public class ContainerActivity extends AppCompatActivity {
     private AFragment aFragment;
     private BFragment bFragment;
     private Button mBtnChange;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_container);
-
+        /*
+        Fragment有自己的生命周期；
+        Fragment依赖于Activity；
+        Fragment通过getActivity()可以获取所在的Activity；Activity通过FragmentManager的findFragmentById()或findFragmentByTag()获取Fragment；
+        Fragment和Activity是多对多的关系。
+        * */
         mBtnChange = findViewById(R.id.btn_change);
         mBtnChange.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,7 +37,7 @@ public class ContainerActivity extends AppCompatActivity {
             }
         });
         // 实例化AFragment
-        aFragment = new AFragment();
+        aFragment = AFragment.newInstance("我是参数");
         // 把AFragment添加到Activity中, 记得调用commit
         getSupportFragmentManager().beginTransaction().add(R.id.fl_container, aFragment).commitAllowingStateLoss();
     }
